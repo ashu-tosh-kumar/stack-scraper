c081233a60b43019249efe26a7f0ddf5276b0216
Ashutosh <37182127+ashu-tosh-kumar@users.noreply.github.com>
2023-07-15 15:11:59 +0530

    add test for main.py

--- test/conftest.py
+++ test/conftest.py
@@ -3,12 +3,12 @@ test modules"""
 
 import pytest
 
-from src.main import create_app
+from src.main import app as flask_app
 
 
 @pytest.fixture()
 def test_app():
-    app = create_app()
+    app = flask_app
     app.config.update(
         {
             "TESTING": True,
